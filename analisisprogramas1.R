4829f6626ebf2a9b360dac6252e83cfdd723dd8e
jcorrean <j.correa.n@gmail.com>
2022-10-26 22:55:06 -0500

    Update analisisprogramas1.R

--- analisisprogramas1.R
+++ analisisprogramas1.R
@@ -9,15 +9,22 @@ textos <- readtext(listado$dir..)
 
 library(quanteda)
 veamos <- corpus(textos)
+source("~/Documents/GitHub/SoftSkillsUniversityPrograms/SampleAnalysis.R")
+docvars(veamos, "Programa") <- Muestra$Programa
+summary(veamos)
+
 dict <- dictionary(list(Auto.Conciencia = c("emoci", "auto-perción", "fortaleza", "necesidad", "valor", "autoeficacia", "espiritualidad"), 
                         Conciencia.Social = c("perspectiva", "empatía", "diversidad", "respeto"),
                         Toma.de.Decisión = c("identificación", "situación", "análisis", "reflexi", "moral", "ético", "responsabilid"),
                         Auto.gestión = c("Impulso", "control", "auto-gestión", "auto-motivaci", "disciplina", "meta", "habilidad"),
-                        Relacionamiento = c("comunicaci", "compromiso", "relaci", "cooperac", "negociaci", "conflict", "ayuda", "búsqued")))
+                        Relacionamiento = c("lider", "comunicaci", "compromiso", "relaci", "cooperac", "negociaci", "conflict", "ayuda", "búsqued")))
 
 pave <- tokens(veamos) %>% tokens_lookup(dictionary = dict) %>% dfm()
 pave
 paved <- data.frame(pave)
+paved$doc_id <- Muestra$Programa
+names(paved)[1] <- "Programa"
+
 sum(paved$auto.conciencia)
 sum(paved$conciencia.social)
 sum(paved$toma.de.decisión)
@@ -26,6 +33,37 @@ sum(paved$relacionamiento)
 
 Enfoque <- data.frame(Competencias=c("Auto-Conciencia", "Conciencia Social", "Toma de Decisión", "Auto-Gestión", "Relacionamiento"),
                  Frecuencias=c(113, 119, 289, 122, 29))
+library(dplyr)
+newcsv <- paved %>%
+  group_by(Programa) %>%
+  summarise(
+    Autoconciencia = sum(auto.conciencia)
+  )
+
+newcsv2 <- paved %>%
+  group_by(Programa) %>%
+  summarise(
+    Conciencia_Social = sum(conciencia.social)
+  )
+
+newcsv3 <- paved %>%
+  group_by(Programa) %>%
+  summarise(
+    TomaDecision = sum(toma.de.decisión)
+  )
+
+newcsv4 <- paved %>%
+  group_by(Programa) %>%
+  summarise(
+    Autogestion = sum(auto.gestión)
+  )
+
+newcsv5 <- paved %>%
+  group_by(Programa) %>%
+  summarise(
+    Relacionamiento = sum(relacionamiento)
+  )
+
 library(ggplot2)
 ggplot(data=Enfoque, aes(x=reorder(Competencias, -Frecuencias), y=Frecuencias)) +
   geom_bar(stat="identity", fill = "red") + geom_text(aes(label=Frecuencias), vjust=1.6, color="white", size=3.5)+
@@ -34,25 +72,43 @@ ggplot(data=Enfoque, aes(x=reorder(Competencias, -Frecuencias), y=Frecuencias)) 
 
 summary(veamos)
 veamos2 <- dfm(veamos, remove = stopwords("spanish"), remove_punct = TRUE, remove_numbers = TRUE)
+
 topfeatures(veamos2, 20)
 red <- as.matrix(veamos2)
 red_data <- data.frame(red)
 palabras <- data.frame(variable.names(red_data))
+
+library(quanteda.textstats)
+freq_weight <- textstat_frequency(veamos2, n = 20)
+freq_weight <- freq_weight %>% filter(!row_number() %in% c(1, 2, 4, 6, 7, 9, 12, 13, 15, 17, 18, 20))
+
+
+ggplot(data = freq_weight, aes(x = nrow(freq_weight):1, y = frequency)) +
+  geom_point() +
+  facet_wrap(~ group, scales = "free") +
+  coord_flip() +
+  scale_x_continuous(breaks = nrow(freq_weight):1,
+                     labels = freq_weight$feature) +
+  labs(x = NULL, y = "Frecuencia")
+
 library(tidyverse)
 Terminos <- red_data %>% select(matches("emocio|percepc|fortalez|necesid|valo|eficacia|espiritualidad|perspectiva|empatía|diversidad|respeto|identificación|situación|análisis|reflexi|moral|ético|responsabilid|Impulso|control|auto-gestión|auto-motivaci|disciplina|meta|habilidad|comunicaci|compromiso|relaci|cooperac|negociaci|conflict|ayuda|búsqued"))
+int <- as.matrix(Terminos)
 
 library(igraph)
-bn <- graph.incidence(Terminos)
+
+bn <- graph.incidence(int)
+summary(bn)
 shapes <- c("circle","square")
 colors <- c("blue","red")
 #Azules son programas, Rojos son Enfoques
-plot(bn,vertex.color=colors[V(bn)$type+1],
+plot(bn,vertex.color=c("lightgreen","red")[V(bn)$type+1],
      vertex.shape=shapes[V(bn)$type+1],
      vertex.size=2,
      vertex.label.dist=1.2, vertex.label=NA)
 
 
-plot(bn, layout = layout_nicely,
+plot(bn, layout = layout_randomly,
      vertex.color=c("lightgreen","red")[V(bn)$type+1], 
      vertex.size=4,
      vertex.label=NA)
