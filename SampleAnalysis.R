a63881876d6300c6d513aec9908fa998cbdaa0ee
Juan C. Correa <j.correa.n@gmail.com>
2023-02-17 12:46:56 -0600

    Update SampleAnalysis.R

--- SampleAnalysis.R
+++ SampleAnalysis.R
@@ -6,3 +6,10 @@ library(tidyverse)
 Muestra <- mutate(Muestra, Programa = ifelse(grepl("Especializac", NOMBRE_DEL_PROGRAMA), "Especialización",
                                       ifelse(grepl("Maestr", NOMBRE_DEL_PROGRAMA), "Maestría", "Doctorado")))
 table(Muestra$Programa)
+institution <- data.frame(table(Muestra$NOMBRE_INSTITUCIÓN))
+Sector <- data.frame(table(Muestra$SECTOR))
+
+Officials <- data.frame(subset(Muestra, Muestra$SECTOR == "Oficial"))
+LevelsOfficials <- data.frame(table(Officials$Programa))
+Private <- data.frame(subset(Muestra, Muestra$SECTOR == "Privado"))
+LevelsPrivate <- data.frame(table(Private$Programa))
