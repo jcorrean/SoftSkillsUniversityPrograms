165380779f079d6952b8274c6522851d4933d9dd
Juan C. Correa <j.correa.n@gmail.com>
2023-02-27 20:23:41 -0600

    Update PreProcessing.R

--- PreProcessing.R
+++ PreProcessing.R
@@ -77,5 +77,3 @@ TODAS <- rbind(crear, innovar, acercar, analizar, apreciar, argumentar, ayudar, 
 colnames(aja)[1]  <- "docname"  
 library(dplyr)
 TODAS2 <- TODAS %>% select(-from, -to, -pre, -post, -pattern) %>% left_join(aja, by = "docname") 
-
-rm(list=setdiff(ls(), "TODAS"))
