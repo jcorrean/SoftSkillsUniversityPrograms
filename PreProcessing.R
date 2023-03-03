4fc2572b26c0e1e1f3bf9d2ab5587cd2aacec686
Juan C. Correa <j.correa.n@gmail.com>
2023-03-03 16:10:09 -0600

    Update PreProcessing.R

--- PreProcessing.R
+++ PreProcessing.R
@@ -86,7 +86,7 @@ flex <- data.frame(kwic(Textos, pattern = "flexibilidad"))
 rm(institution, LevelsOfficials, LevelsPrivate, listado, Muestra, Officials, Private, Sector, textos, Textos, DATA_DIR)
 
 
-TODAS <- rbind(decidir, sp, pc, creatividad, paciencia, crear, innovar, acercar, analizar, apreciar, argumentar, ayudar, cambiar, compartir, competir,
+TODAS <- rbind(flex, td, decidir, sp, pc, creatividad, paciencia, crear, innovar, acercar, analizar, apreciar, argumentar, ayudar, cambiar, compartir, competir,
                comprender, comprometer, comprometerse, comunicar, conflicto, controlar, cooperar, dirigir,
                empatia, equipo, etico, evaluar, fomentar, fortalecer, generar, gestionar, identificar, impulsar,
                interactuar, liderar, manifestar, motivar, negociar, orientar, planificar, reconocer, reflexionar, 
