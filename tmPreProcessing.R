5c5b59bb9f6c9a344d4a8a89452c603d1bfc426b
Juan C. Correa <j.correa.n@gmail.com>
2023-03-05 20:16:31 -0600

    Update tmPreProcessing.R

--- tmPreProcessing.R
+++ tmPreProcessing.R
@@ -9,13 +9,25 @@ DirSource()
 DATA_DIR <- system.file("extdata/", package = "readtext")
 textos <- readtext(listado$dir..)
 textos$doc_id <- gsub("[^0-9-]", "", textos$doc_id)
-Programs <- DataframeSource(textos)
-Programs <- removeNumbers(Programs)
-x <- Corpus(Programs)
-P <- tm_map(x, stripWhitespace)
-P <- tm_map(x, content_transformer(tolower))
-P <- tm_map(x, removeWords, stopwords("spanish"))
 
-inspect(x)
+Programs <- DataframeSource(textos)
+x <- Corpus(Programs)
+P <- tm_map(x, content_transformer(tolower))
+P <- tm_map(x, stripWhitespace)
+P <- tm_map(x, removeWords, stopwords("spanish"))
+uu<- content_transformer(function(x, ...) {gsub("\\(\\w+\\)", "", x)})
+P <- tm_map(x, uu)
+P <- tm_map(x, removeWords, c("académico", "académicos", "académica", "académicas", "con", "desde"))
+P <- tm_map(x, removeWords, stopwords(kind = "es"))
+
+
+
+dtm <- DocumentTermMatrix(P)
+dtm
+terminos <- data.frame(findFreqTerms(dtm))
+dtm2 <- removeSparseTerms(dtm, 0.90)
+dtm3 <- as.matrix(dtm2) # At least here we have a truly adjacency matrix
+stopwords(kind = "es")
+
 
 
