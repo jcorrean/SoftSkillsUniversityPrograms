7bf1dfaef2240ad2ddd5a0d6041ef534edf9863a
Juan C. Correa <j.correa.n@gmail.com>
2023-03-07 15:31:21 -0600

    Update Analysis by Program Level.R

--- Analysis by Program Level.R
+++ Analysis by Program Level.R
@@ -1,5 +1,7 @@
 # Analysis by Program Level
-
+#Spec <- Spec %>% select(., "docname", "keyword")
+#MS <- MS %>% select(., "docname", "keyword")
+#PhD <- PhD %>% select(., "docname", "keyword")
 setwd("/home/jc/Documents/Paper Soft Skills Sampled Programs")
 listado <- data.frame(dir())
 library(readtext)
@@ -18,22 +20,27 @@ source("~/Documents/GitHub/SoftSkillsUniversityPrograms/SampleAnalysis.R")
 docvars(Textos, "Programa") <- Muestra$NOMBRE_DEL_PROGRAMA
 docvars(Textos, "Program.Level") <- Muestra$`Academic Level`
 docvars(Textos, "Institution") <- Muestra$NOMBRE_INSTITUCIÓN
+docvars(Textos, "Accreditation") <- Muestra$Accreditation
+
 summary(Textos)
 aja <- data.frame(summary(Textos, n = length(Textos)))
 
 SPEC <- corpus_subset(Textos, Program.Level == "Specialization")
 MS <- corpus_subset(Textos, Program.Level == "Masters")
 PhD <- corpus_subset(Textos, Program.Level == "Doctorate")
+QC <- corpus_subset(Textos, Accreditation == "Qualified Certification")
+HQC <- corpus_subset(Textos, Accreditation == "High-Quality Certification")
 
+# Specialization Programs
 library(quanteda)
 TOKS <- tokens(SPEC, remove_numbers = TRUE, remove_punct = TRUE) %>% 
   tokens_remove(stopwords("es"))
 DTM <- dfm(TOKS, tolower = FALSE)
-SoftSkills <- c("generar", "evaluar", "liderar", "equipos", "analizar", "gestionar", "fortalecer", "identificar", "crear", "comprender") #Eigenvector
-toks_inside <- tokens_keep(TOKS, pattern = SoftSkills, window = 0)
-DTM2 <- dfm(toks_inside)
+#SoftSkills <- c("generar", "evaluar", "liderar", "equipos", "analizar", "gestionar", "fortalecer", "identificar", "crear", "comprender") #Eigenvector
+#toks_inside <- tokens_keep(TOKS, pattern = SoftSkills, window = 0)
+#DTM2 <- dfm(toks_inside)
 
-DTM3 <- as.matrix(DTM2)
+DTMSpec <- as.matrix(DTM)
 colnames(DTM3)
 
 library(bipartite)
