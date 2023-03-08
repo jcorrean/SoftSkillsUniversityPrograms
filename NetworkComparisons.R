874fc9101e8d11ac044f7f99cf2b8603fe0c2c91
Juan C. Correa <j.correa.n@gmail.com>
2023-03-08 03:10:46 -0600

    Update NetworkComparisons.R

--- NetworkComparisons.R
+++ NetworkComparisons.R
@@ -3,24 +3,32 @@ load("~/Documents/GitHub/SoftSkillsUniversityPrograms/ResultsbyProgram.RData")
 load("~/Documents/GitHub/SoftSkillsUniversityPrograms/ResultsbyAccreditation.RData")
 
 
-ggplot(Centralities, aes(x = Eigen.vector, y = Level, fill = Level)) +
+p1 <- ggplot(Centralities, aes(x = Eigen.vector, y = Level, fill = Level)) +
   geom_density_ridges(alpha = 0.3) +
   theme_ridges() + 
   theme(legend.position = "none") + 
   xlab("Eigenvector Centrality") + 
   ylab("Academic Program") + 
-  theme(axis.text.x=element_text(size=15)) +
-  theme(axis.text.y=element_text(size=15)) +
-  theme(axis.title.x=element_text(face="italic", colour="black", size=20)) +
-  theme(axis.title.y=element_text(face="italic", colour="black", size=20))
+  theme(axis.text.x=element_text(size=35)) +
+  theme(axis.text.y=element_text(size=35)) +
+  theme(axis.title.x=element_text(face="italic", colour="black", size=35)) +
+  theme(axis.title.y=element_text(face="italic", colour="black", size=35))
 
 
-ggplot(Centralities2, aes(x = Eigen.vector, y = Accreditation, fill = Accreditation)) +
+p2 <- ggplot(Centralities2, aes(x = Eigen.vector, y = Accreditation, fill = Accreditation)) +
   geom_density_ridges(alpha = 0.3) +
   theme_ridges() + 
   theme(legend.position = "none") + 
   xlab("Eigenvector Centrality") + 
-  ylab("Type of Accreditation")
+  ylab("Type of Accreditation") + 
+  theme(axis.text.x=element_text(size=35)) +
+  theme(axis.text.y=element_text(size=35)) +
+  theme(axis.title.x=element_text(face="italic", colour="black", size=35)) +
+  theme(axis.title.y=element_text(face="italic", colour="black", size=35))
+
+
+#library(ggpubr)
+#ggarrange(p1, p2, labels = c("(A)", "(B)"), ncol = 1, nrow = 2)
 
 library(psych)
 ByProgram <- psych::describe.by(Centralities$Eigen.vector, group = Centralities$Level, mat = TRUE)
