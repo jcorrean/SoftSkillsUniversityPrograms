9684c94026f30386d172f79b422bf6c29cf0459a
Juan C. Correa <j.correa.n@gmail.com>
2023-08-16 01:23:00 -0600

    Update Figure4B.R

--- Figure4B.R
+++ Figure4B.R
@@ -1,16 +1,17 @@
-load("~/Documents/GitHub/SoftSkillsUniversityPrograms/DataForFigure4.RData")
-rm(list=setdiff(ls(), "SkillsProgramsCentrality"))
+load("DataForFigure4B.RData")
+rm(list=setdiff(ls(), "Gen"))
+Gen <- Gen[1:4]
 
 library(psych)
-pairs.panels(SkillsProgramsCentrality, 
+pairs.panels(Gen, 
              method = "spearman", 
              hist.col = "green",
              density = TRUE,  
              ellipses = TRUE,
-             pch = 21,
-             cex = 2.5,
+             pch = 15,
+             cex = 1.5,
              cex.axis = 1.8,
-             cex.labels = 4.5,
+             cex.labels = 2.5,
              lwd = 2,
              rug = TRUE,
              stars = TRUE
