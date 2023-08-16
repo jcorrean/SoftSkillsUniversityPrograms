b7363770300a5fbcf268b9bd1c9e168bcac4dc70
Juan C. Correa <j.correa.n@gmail.com>
2023-08-16 01:08:46 -0600

    Update Analysis by Program Level.R

--- Analysis by Program Level.R
+++ Analysis by Program Level.R
@@ -8,16 +8,18 @@ MasterPrograms <- TODAS2 %>% filter(., Program=="Master") %>%
   select(., docname, keyword)
 DoctoratePrograms <- TODAS2 %>% filter(., Program == "Doctorate") %>% 
   select(., docname, keyword)
-
+General <- TODAS2 %>% select(., docname, keyword)
 
 library(igraph)
 BNS <- graph.data.frame(SpecPrograms, directed = FALSE)
 BNM <- graph.data.frame(MasterPrograms, directed = FALSE)
 BND <- graph.data.frame(DoctoratePrograms, directed = FALSE)
+BNA <- graph.data.frame(General, directed = FALSE)
 
 hist(degree_distribution(BNS))
 hist(degree_distribution(BNM))
 hist(degree_distribution(BND))
+hist(degree_distribution(BNA))
 
 Spec <- data.frame(Degree = igraph::degree(BNS),
                                        Closeness = igraph::closeness(BNS),
@@ -47,9 +49,21 @@ Doc <- data.frame(Degree = igraph::degree(BND),
                  Eigen = igraph::eigen_centrality(BND))
 Doc <- Doc[ -c(5:25) ]
 rownames(Doc)
-Doc <- Doc[26:58,]
 Doc$SS <- rownames(Doc)
+Doc <- Doc[order(Doc$SS), ]
+Doc <- Doc[31:60,]
 Doc$Level <- "Doctorate"
+
+Gen <- data.frame(Degree = igraph::degree(BNA),
+                   Closeness = igraph::closeness(BNA),
+                   Betweennes = igraph::betweenness(BNA),
+                   Eigen = igraph::eigen_centrality(BNA))
+Gen <- Gen[ -c(5:25) ]
+rownames(Gen)
+Gen$SS <- rownames(Gen)
+Gen <- Gen[order(Gen$SS), ]
+Gen <- Gen[204:246,]
+
 
 Centralities <- list(Spec, MS, Doc)
 Centralities <- as.data.frame(do.call(rbind, Centralities))
@@ -58,11 +72,6 @@ Resumen <- data.frame(table(Centralities$SS))
 
 library(ggridges)
 library(ggplot2)
-
-# Diamonds dataset is provided by R natively
-#head(diamonds)
-
-# basic example
 ggplot(Centralities, aes(x = Eigen.vector, y = Level, fill = Level)) +
   geom_density_ridges(alpha = 0.3) +
   theme_ridges() + 
@@ -75,21 +84,22 @@ ggplot(Centralities, aes(x = Eigen.vector, y = Level, fill = Level)) +
   theme(axis.title.y=element_text(face="italic", colour="black", size=20))
 
 
-IM <- as_incidence_matrix(BNS, names = TRUE, sparse = TRUE, types = bipartite_mapping(BNS)$type)
+IM <- as_incidence_matrix(BNA, names = TRUE, sparse = TRUE, types = bipartite_mapping(BNA)$type)
 IM2 <- as.matrix(IM)
 
-bipartite.mapping(BNS)
-V(BNS)$type <- bipartite_mapping(BNS)$type
-V(BNS)$color <- ifelse(V(BNS)$type, "red", "green")
-V(BNS)$shape <- ifelse(V(BNS)$type, "circle", "square")
-V(BNS)$label.cex <- ifelse(V(BNS)$type, 0.8, 1)
-V(BNS)$size <- sqrt(igraph::degree(BNS))
-E(BNS)$color <- "lightgrey"
-  plot(BNS, 
+bipartite.mapping(BNA)
+V(BNA)$type <- bipartite_mapping(BNA)$type
+V(BNA)$color <- ifelse(V(BNA)$type, "red", "green")
+V(BNA)$shape <- ifelse(V(BNA)$type, "circle", "square")
+V(BNA)$label.cex <- ifelse(V(BNA)$type, 0.8, 1)
+V(BNA)$size <- sqrt(igraph::degree(BNA))
+E(BNA)$color <- "lightgrey"
+  plot(BNA, 
        vertex.label = NA, 
-       layout = layout_in_circle, 
+       layout = layout_as_tree, 
        main = "")
 
 library(bipartite)
 plotweb(IM2, method = "normal", col.high = "lightgreen", col.low = "pink", col.interaction = "lightgrey")
 bipartite::visweb(IM2)
+save.image("DataForFigure4B.RData")
