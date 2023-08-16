898eeac35c960acec68e72b67b4b8c0546cdcd8b
Juan C. Correa <j.correa.n@gmail.com>
2023-08-16 09:41:33 -0600

    Update quantedaPreProcessing.R

--- quantedaPreProcessing.R
+++ quantedaPreProcessing.R
@@ -1,14 +1,6 @@
 # DTM by program level from quanteda 
-setwd("/home/jc/Documents/Paper Soft Skills Sampled Programs")
-listado <- data.frame(dir())
-library(readtext)
-library(tm)
-DirSource()
-# Get the data directory from readtext
-DATA_DIR <- system.file("extdata/", package = "readtext")
-textos <- readtext(listado$dir..)
-textos$doc_id <- gsub("[^0-9-]", "", textos$doc_id)
-
+load("PreProcessedData.RData")
+rm(list=setdiff(ls(), "textos"))
 library(quanteda)
 TOKS <- corpus(textos$text) %>% 
   tokens(remove_numbers = TRUE, remove_punct = TRUE) %>% 
